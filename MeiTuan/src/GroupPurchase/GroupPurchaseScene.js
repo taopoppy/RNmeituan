e5633a8e9b03ffcf6f2f3280e080b42648867743
taopoppy <38676845+taopoppy@users.noreply.github.com>
2018-07-10 11:29:25 +0800

    thirteenth commit

--- MeiTuan/src/GroupPurchase/GroupPurchaseScene.js
+++ MeiTuan/src/GroupPurchase/GroupPurchaseScene.js
@@ -11,13 +11,18 @@ import {
     View,
     Text,
     Image,
+    InteractionManager,
 } from 'react-native';
 import NavigationItem from '../widget/NavigationItem';
-import {Heading1,Heading2,Paragraph} from '../widget/Text';
+import {Heading1,Heading2,Heading3,Paragraph} from '../widget/Text';
 import screen from '../common/screen';
 import color from '../widget/color';
 import Button from '../widget/Button';
 import Separator from '../widget/Separator';
+import api,{recommendUrlWithId, groupPurchaseDetailWithId} from '../api';
+import RefreshListView,{RefreshState} from 'react-native-refresh-list-view';
+import SpacingView from '../widget/SpacingView';
+import GroupPurchaseCell from './GroupPurchaseCell';
 
 export default class GroupPurchaseScene extends PureComponent{
     static navigationOptions=({navigation})=>({
@@ -34,40 +39,110 @@ export default class GroupPurchaseScene extends PureComponent{
     })
     constructor(props){
         super(props)
-        //alert(JSON.stringify(this.props.navigation.state.params.info))
+        this.info=this.props.navigation.state.params.info;
+        this.state={
+            data:[],
+            refreshState:RefreshState.Idle,  //列表刷新状态，加载成功（普通状态）
+        }
     }
-    render(){
-        let {info}=this.props.navigation.state.params;
-        let imageUrl=info.imageUrl.replace('w.h','480.0');
+    componentDidMount(){
+        InteractionManager.runAfterInteractions(()=>{
+            this.requestData();
+        })
+    }
+    requestData(){
+        this.requestRecommend();
+    }
+    requestRecommend=async()=>{
+        try {
+            this.setState({refreshState:RefreshState.HeaderRefreshing}) //开始下拉刷新
+            let info =this.info;
+            let response=await fetch(recommendUrlWithId(info.id));
+            let json=await response.json();
 
-        return <View style={styles.container}>
+            let dataList=json.data.deals.map((info)=>{
+                return {
+                    id:info.id,
+                    imageUrl:info.imgurl,
+                    title:info.brandname,
+                    subtitle:`[${info.range}]${info.title}`,
+                    price:info.price
+                }
+            })
+
+            this.setState({
+                data:dataList,
+                refreshState:RefreshState.NoMoreData,//加载全部的数据
+            })
+        } catch (error) {
+            this.setState({
+                refreshState:RefreshState.Failure,//加载数据失败
+            })
+        }
+    }
+    renderCell=(rowData)=>{
+        return (
+            <GroupPurchaseCell
+                info={rowData.item}
+                onPress={()=>{
+                   this.props.navigation.navigate('GroupPurchaseScene',{info:rowData.item})
+                }}
+            />
+        )
+    }
+    renderHeader(imageUrl,price){
+        return (
             <View>
-                <Image
-                    source={{uri:imageUrl}}
-                    style={styles.banner}
-                />
-                <View style={styles.TopContainer}>
-                    <Heading2 style={{color:color.primary}}>¥</Heading2>
-                    <Heading1 style={{marginBottom:-8}}>{info.price}</Heading1>
-                    <Paragraph style={{marginLeft:10}}>门市价：¥{(info.price*1.1).toFixed(0)}</Paragraph> 
-                    <View style={{flex:1}}/>
-                    <Button 
-                        title={'立即抢购'} 
-                        tilteStyle={{color:'white',fontSize:18}}
-                        style={styles.buyButton}
+                <View>
+                    <Image
+                        source={{uri:imageUrl}}
+                        style={styles.banner}
                     />
+                    <View style={styles.TopContainer}>
+                        <Heading2 style={{color:color.primary}}>¥</Heading2>
+                        <Heading1 style={{marginBottom:-8}}>{price}</Heading1>
+                        <Paragraph style={{marginLeft:10}}>门市价：¥{(price * 1.1).toFixed(0)}</Paragraph> 
+                        <View style={{flex:1}}/>
+                        <Button 
+                            title={'立即抢购'} 
+                            tilteStyle={{color:'white',fontSize:18}}
+                            style={styles.buyButton}
+                        />
+                    </View>
+                </View>
+
+                <Separator/>
+
+                <View style={styles.tagContainer}>
+                    <Image style={{width:20,height:20}} source={require('../img/home/icon_deal_anytime_refund.png')}/>
+                    <Paragraph style={{color:'#89B24F'}}> 随时退</Paragraph>
+                    <View style={{flex:1}}/>
+                    <Paragraph>已售123</Paragraph>
+                </View>
+
+                <SpacingView />
+                <View style={styles.tipHeader}>
+                    <Heading3>看了本团购的用户还看了</Heading3>
                 </View>
             </View>
-
-            <Separator/>
-
-            <View style={styles.tagContainer}>
-                <Image style={{width:20,height:20}} source={require('../img/home/icon_deal_anytime_refund.png')}/>
-                <Paragraph style={{color:'#89B24F'}}> 随时退</Paragraph>
-                <View style={{flex:1}}/>
-                <Paragraph>已售123</Paragraph>
+        )
+    }
+    render(){
+        let info=this.info;
+        let imageUrl=info.imageUrl.replace('w.h','480.0');
+        let price=info.price;
+        return(
+            <View style={styles.container}>
+                <RefreshListView
+                    data={this.state.data}      //同FlatList中的data属性
+                    renderItem={this.renderCell}           //每一行的渲染方式
+                    ListHeaderComponent={()=>this.renderHeader(imageUrl,price)}   //头部组件(头部组件中好像找不到this.props.navigation)
+                    keyExtractor={(item,index)=>item.id+''}    //每一行都有唯一的key
+                    refreshState={this.state.refreshState}   //更新状态
+                    onHeaderRefresh={()=>this.requestData()}      //下来刷新的回调函数
+                />
             </View>
-       </View>
+        )
     }
 }
 
@@ -97,8 +172,20 @@ const styles=StyleSheet.create({
         height:36,
         borderRadius:7,
     },
+    tipHeader: {
+        height: 35,
+        justifyContent: 'center',
+        borderWidth: screen.onePixel,
+        borderColor: color.border,
+        paddingVertical: 8,
+        paddingLeft: 20,
+        backgroundColor: 'white'
+    }
     
 })
 
 
 //(number).toFixed(0) 保留整数
+
+//如果是这样写的onHeaderRefresh={()=>this.requestData()}  回调函数可以这样写requestData(){}
+//如果是这样写的onHeaderRefresh={this.requestData}   回调函数应该这样写   requestData = () => {}
